d1e01dd80046e07184114b9adf0acd2fa4b0003d
PhRezende-eng <ph.hr.001@gmail.com>
2021-04-10 22:59:01 -0300

    Created and DONE the api rest

--- lib/repository/contatos_repository.dart
+++ lib/repository/contatos_repository.dart
@@ -2,23 +2,21 @@ import 'package:dio_node_js/model/contato_model.dart';
 import 'package:dio_node_js/utils/custom_dio.dart';
 
 class ContatosRepository {
-  Future<List<ContatoModel>> finAll() {
+  Future<List<ContatoModel>> findAll() {
     var _dio = CustomDio.withAuthentication().instance;
     return _dio.get('http://localhost:3000/contatos').then((res) {
-      return res.data
-          .map<ContatoModel>((c) => ContatoModel.fromMap(c))
-          .toList();
+      return res.data.map<ContatoModel>((c) => ContatoModel.fromMap(c)).toList()
+          as List<ContatoModel>;
     });
   }
 
-  Future<List<ContatoModel>> finFilter(String nome) {
+  Future<List<ContatoModel>> findFilter(String nome) {
     var _dio = CustomDio.withAuthentication().instance;
     return _dio
         .get('http://localhost:3000/contatos/filtrar?nome=$nome')
         .then((res) {
-      return res.data
-          .map<ContatoModel>((c) => ContatoModel.fromMap(c))
-          .toList();
+      return res.data.map<ContatoModel>((c) => ContatoModel.fromMap(c)).toList()
+          as List<ContatoModel>;
     });
   }
 }
